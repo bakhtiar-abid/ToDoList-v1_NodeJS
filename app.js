1c424dd5c58ad93c4e9c97da6424a351273695c8
bakhtiarabid <abid.bu16@gmail.com>
2021-05-18 10:46:22 +0600

    Todo List

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,16 +13,16 @@ app.get("/", function (req, res) {
 //    res.send("Hello");
  
    
-var today = new Date();
+let today = new Date();
 
-var options = {
+let options = {
    weekday: "long",
    day: "numeric",
    month: "long",
    year: "numeric",
 };
 
-var day = today.toLocaleDateString("en-US", options);
+let day = today.toLocaleDateString("en-US", options);
 
 res.render("list", { kindOfDay: day, newListItems: items });
 
