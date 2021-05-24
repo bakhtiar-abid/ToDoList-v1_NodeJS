b51022f1fa9400b41e94d3964a1d66b2ce39ad42
bakhtiarabid <abid.bu16@gmail.com>
2021-05-24 12:54:41 +0600

    Added Date

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -13,20 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-   let today = new Date();
-
-   let options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-   };
-
-   let day = today.toLocaleDateString("en-US", options);
+  
+        let day = date();
 
    res.render("list", { listTitle: day, newListItems: items });
 
-});
+});  
 
 
 
